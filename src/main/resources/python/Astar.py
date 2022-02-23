0301b84d6e34e34ea2ecaf721e70e09ab3ee6f69
MINSOO SUN <65339890+tjsalstn33@users.noreply.github.com>
2022-02-23 14:22:41 -0500

    Update Astar.py

--- src/main/resources/python/Astar.py
+++ src/main/resources/python/Astar.py
@@ -1,5 +1,4 @@
 import sys
-
 import cv2 as cv
 import json
 import numpy as np
@@ -11,25 +10,27 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 UL = sys.argv[1]
 RL = sys.argv[2]
 
-
 def read_img(file_path):
-    temp = []  #
-    blueprint = []  # 0 is path, 1is block
-    base64_data = open(file_path, 'r').read()
+    temp = [] #
+    blueprint = [] #0 is path, 1is block
+    base64_data = open(file_path,'r').read()
     b = base64.b64decode(base64_data)
-    nparr = np.fromstring(b, np.uint8)
-    img = cv.imdecode(nparr, cv.IMREAD_COLOR)
+    nparr = np.fromstring(b,np.uint8)
+    img = cv.imdecode(nparr,cv.IMREAD_COLOR)
     Img = img.tolist()
-    X, Y = len(Img[1]), len(Img)
+    X,Y = len(Img[1]),len(Img)
     for y in range(Y):
         for x in range(X):
-            if Img[y][x] == [255, 255, 255]:
+            if Img[y][x] == [255,255,255]:
                 temp.append(0)
             else:
                 temp.append(1)
         blueprint.append(temp)
     temp = []
     return blueprint
+
+    
+    
 
 
 class Node:
@@ -44,7 +45,7 @@ class Node:
         return self.position == other.position
 
 
-def heuristic(node, goal):
+def heuristic(node, goal):  
     dx = abs(node.position[0] - goal.position[0])
     dy = abs(node.position[1] - goal.position[1])
     return dx + dy
@@ -52,21 +53,21 @@ def heuristic(node, goal):
 
 def UseRomListist(Ustr):
     User_List = []
-    Ustr = Ustr.replace('[', '')
-    Ustr = Ustr.replace(']', '')
+    Ustr = Ustr.replace('[','')
+    Ustr = Ustr.replace(']','')
     Ustr = Ustr.split(',')
     for i in Ustr:
-        User_List.append(tuple(map(int, i.split(':'))))
+        User_List.append(tuple(map(int,i.split(':'))))
     return User_List
 
 
 def RoomList(Rstr):
     Room_List = []
-    Rstr = Rstr.replace('[', '')
-    Rstr = Rstr.replace(']', '')
+    Rstr = Rstr.replace('[','')
+    Rstr = Rstr.replace(']','')
     Rstr = Rstr.split(',')
     for i in Rstr:
-        Room_List.append(tuple(map(int, i.split(':'))))
+       Room_List.append(tuple(map(int,i.split(':'))))
     return Room_List
 
 
@@ -74,12 +75,12 @@ def Astar(maze, start, end):
     # Create start and end node
     startNode = Node(None, start)
     endNode = Node(None, end)
-    # Initialize both open and closed list
+     # Initialize both open and closed list
     openList = []
     closedList = []
     # Add the start node
     openList.append(startNode)
-    # Loop until you find the end
+    #Loop until you find the end
     while openList:
         # Get the current node
         currentNode = openList[0]
@@ -99,7 +100,7 @@ def Astar(maze, start, end):
                 path.append(current.position)
                 current = current.parent
             return path[::-1]  # reverse
-        # Generate children
+         # Generate children
         children = []
         # 인접한 xy좌표 전부
         for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
@@ -107,13 +108,13 @@ def Astar(maze, start, end):
 
             nodePosition = (
                 currentNode.position[0] + newPosition[0],  # X
-                currentNode.position[1] + newPosition[1])  # Y
+                currentNode.position[1] + newPosition[1])  # Y       
             within_range_criteria = [
                 nodePosition[0] > (len(maze) - 1),
                 nodePosition[0] < 0,
                 nodePosition[1] > (len(maze[len(maze) - 1]) - 1),
                 nodePosition[1] < 0,
-                ]
+            ]
             if any(within_range_criteria):  # 하나라도 true면 범위 밖임
                 continue
             # 장애물이 있으면 다른 위치 불러오기
@@ -136,37 +137,61 @@ def Astar(maze, start, end):
 
 
 def Compare():
-    RomList = RoomList(RL)
-    end = RomList[0][0], RomList[0][1]
-    userX, userY = UseRomListist(UL)[0]
+    RomL = []
+    RomList  = RoomList(RL)
+    end = RomList[0][0],RomList[0][1]
+    RomL.append(end)
+    userX,userY = UseRomListist(UL)[0]
     temp = abs(userX - RomList[0][0]) + abs(userY - RomList[0][1])
-    for i in range(1, len(RomList)):
-        if temp > abs(userX - RomList[i][0]) + abs(userY - RomList[i][1]):
-            end = RomList[i][1], RomList[i][0]
-    return end
-
+    for i in range(1,len(RomList)):
+        if temp>abs(userX - RomList[i][0]) + abs(userY - RomList[i][1]):
+            end = RomList[i][1],RomList[i][0]
+            RomL.append(end)
+            if len(RomL) >2:
+                break
+    return RomL
 
 def startAstar(file_path):
     ax = []
     ay = []
     data = []
     rpath = []
+    fstpath = []
+    w = 0
+    comp = 0
     maze = read_img(file_path)
-    start = UseRomListist(UL)[0][1], UseRomListist(UL)[0][0]
-    # start = y,x
-    end = Compare()
-    # end = y,x
-    path = Astar(maze, start, end)
+    start = UseRomListist(UL)[0][1],UseRomListist(UL)[0][0]
+    end = Compare()[0]
+    path = Astar(maze,start,end)
     for i in path:
-        rpath.append(tuple(4 * elem for elem in i))
-    for i in rpath:
+        fstpath.append(tuple(4*elem for elem in i))
+    user = UseRomListist(UL)
+    for i in user:
+        w +=fstpath.count(i)
+    comp = w + len(fstpath)
+    w = 0
+    for i in range(1,len(Compare())):
+        end = Compare()[i]
+        path = Astar(maze,start,end)
+        for i in path:
+            rpath.append(tuple(4*elem for elem in i))
+        user = UseRomListist(UL)
+        for i in user:
+            w +=fstpath.count(i)
+        if comp> w+len(rpath):
+            answer = rpath
+        else:
+            answer = fstpath
+            
+            
+        
+        
+    for i in answer:
         ax.append(i[1])
         ay.append(i[0])
     for i in range(len(ax)):
-        data.append({'x': ax[i], 'y': ay[i]})
-    return (json.dumps(data, ensure_ascii=False, indent='\t'))
-
-
+        data.append({'x':ax[i],'y':ay[i]})
+    return (json.dumps(data,ensure_ascii=False,indent='\t'))
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
     file_path = sys.argv[3]
